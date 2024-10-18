d81b68f4167b58955040e31c1e29bba9afef449b
Bruno Martins <bgcngm@gmail.com>
2024-10-18 18:36:48 +0100

    salami: Adopt Python extract utils
    Change-Id: Ie9d629ab91bae60a1b9a60e62477de60ea2973f6

---
+++ extract-files.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'vendor/oneplus/sm8550-common',
+]
+
+blob_fixups: blob_fixups_user_type = {
+    'odm/etc/camera/CameraHWConfiguration.config': blob_fixup()
+        .regex_replace('SystemCamera =  0;  0;  0;  1;  0;  1;', 'SystemCamera =  0;  0;  0;  0;  0;  0;')
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'salami',
+    'oneplus',
+    namespace_imports=namespace_imports,
+    blob_fixups=blob_fixups,
+    check_elf=True,
+    add_firmware_proprietary_file=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'sm8550-common', module.vendor
+    )
+    utils.run()
