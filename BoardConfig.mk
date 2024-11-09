367a1d92999806506512b27a573c2804bb8e9e92
pjgowtham <pjgowtham@gmail.com>
2024-11-09 10:26:21 +0530

    Revert "lemonadep: We now run at 1080p by default ^.^"
    This reverts commit 67e305524218ef1cd9e68d678336173cff2812a8.
    
    Change-Id: I349159718e74b8231e7ede2cf6d4b4f0bc31cd0f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,9 +12,6 @@ DEVICE_PATH := device/oneplus/salami
 # Assert
 TARGET_OTA_ASSERT_DEVICE := OP591BL1,OP594DL1
 
-# Display
-TARGET_SCREEN_DENSITY := 450
-
 # Kernel
 TARGET_KERNEL_CONFIG += vendor/oplus/salami.config
 
@@ -22,7 +19,8 @@ TARGET_KERNEL_CONFIG += vendor/oplus/salami.config
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
-TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
+TARGET_RECOVERY_DENSITY := xxhdpi
+TARGET_RECOVERY_UI_MARGIN_HEIGHT := 126
 
 # Include the proprietary files BoardConfig.
 include vendor/oneplus/salami/BoardConfigVendor.mk
