5edaf1c329ce24f15f6226de6a2c7200148b5cda
Bruno Martins <bgcngm@gmail.com>
2024-12-13 14:36:52 +0000

    salami: Update from OOS 11.F.21
    Change-Id: I70bbf54e948aec7f68958cb65efb937c67f99447

--- lineage_salami.mk
+++ lineage_salami.mk
@@ -23,8 +23,8 @@ PRODUCT_MODEL := CPH2449
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="CPH2449EEA-user 14 TP1A.220905.001 T.R4T3.1a1ad1b_1-6 release-keys" \
-    BuildFingerprint=OnePlus/CPH2449EEA/OP594DL1:14/TP1A.220905.001/T.R4T3.1a1ad1b_1-6:user/release-keys \
+    BuildDesc="CPH2449EEA-user 15 TP1A.220905.001 T.R4T3.1b9a73a-9883-1ba8a release-keys" \
+    BuildFingerprint=OnePlus/CPH2449EEA/OP594DL1:15/TP1A.220905.001/T.R4T3.1b9a73a-9883-1ba8a:user/release-keys \
     DeviceName=OP594DL1 \
     DeviceProduct=CPH2449 \
     SystemDevice=OP594DL1 \
