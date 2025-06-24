545d20956c024d11289abea1df1ebdab033865bb
Bruno Martins <bgcngm@gmail.com>
2025-06-24 21:44:16 +0100

    salami: audio: Bring in default volume configurations
    Configurations imported from BP1A.
    
    Change-Id: I078a155814087f37445231b4e11c29a3d489f8e6

--- device.mk
+++ device.mk
@@ -8,6 +8,11 @@
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_volumes.xml \
+    $(LOCAL_PATH)/configs/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/default_volume_tables.xml
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2376
 TARGET_SCREEN_WIDTH := 1080
