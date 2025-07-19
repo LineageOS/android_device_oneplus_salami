f9fb21a8a730be25053da444ec6cee04bca48fe1
Michael Bestas <mkbestas@lineageos.org>
2025-07-19 10:55:59 +0300

    salami: Migrate QTI power HAL to soong_config_set
    Change-Id: Ib80c15ee523130011147d378f665592dae501ec2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,9 +21,6 @@ TARGET_SCREEN_DENSITY := 450
 # Kernel
 TARGET_KERNEL_ADDITIONAL_FLAGS += CONFIG_SALAMI_DTB=y
 
-# Power
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
-
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
