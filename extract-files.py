c2346a8740dbda1af1290e35a809ea69bd069684
Bruno Martins <bgcngm@gmail.com>
2025-09-23 21:56:31 +0100

    salami: Load proper NFC config based on project
    Test: Boot, verify that persist.vendor.nfc.config_file_name property is
    properly set and that NFC works.
    Change-Id: I942429c4be2c167ac2018b02d38f73b166e98511

--- extract-files.py
+++ extract-files.py
@@ -41,6 +41,12 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'odm/etc/camera/CameraHWConfiguration.config': blob_fixup()
         .regex_replace('SystemCamera =  0;  0;  0;  1;  0;  1;', 'SystemCamera =  0;  0;  0;  0;  0;  0;'),
+    (
+        'odm/etc/libnfc-mtp-SN100.conf_22811',
+        'odm/etc/libnfc-mtp-SN100.conf_22861'
+    ): blob_fixup()
+        .regex_replace('(NXPLOG_.*_LOGLEVEL)=0x03', '\\1=0x02')
+        .regex_replace('NFC_DEBUG_ENABLED=1', 'NFC_DEBUG_ENABLED=0'),
     'odm/lib64/libAlgoProcess.so': blob_fixup()
         .replace_needed('android.hardware.graphics.common-V3-ndk.so', 'android.hardware.graphics.common-V6-ndk.so'),
     (
@@ -69,9 +75,6 @@ blob_fixups: blob_fixups_user_type = {
     'odm/lib64/libextensionlayer.so': blob_fixup()
         .replace_needed('libziparchive.so', 'libziparchive_odm.so'),
     'vendor/etc/libnfc-nci.conf': blob_fixup()
-        .regex_replace('NFC_DEBUG_ENABLED=1', 'NFC_DEBUG_ENABLED=0'),
-    'vendor/etc/libnfc-nxp.conf': blob_fixup()
-        .regex_replace('(NXPLOG_.*_LOGLEVEL)=0x03', '\\1=0x02')
         .regex_replace('NFC_DEBUG_ENABLED=1', 'NFC_DEBUG_ENABLED=0')
 }  # fmt: skip
 
