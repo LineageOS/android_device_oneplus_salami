2db4d4ad0ff3e1483fc737888d59af07df4b4528
Bruno Martins <bgcngm@gmail.com>
2025-10-26 09:54:52 +0000

    salami: Enable eUICC support
    Change-Id: I5471a1d9a0a11cc343fb65f5aff4dcffc4ef52c4

--- device.mk
+++ device.mk
@@ -32,6 +32,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
+    FrameworksResTargetEuicc \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSettingsResTarget \
@@ -55,6 +56,13 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+
+# Telephony
+PRODUCT_PACKAGES += \
+    OplusEuicc
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
 # Vibrator
 PRODUCT_PACKAGES += \
