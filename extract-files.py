6426547b58bc084e8b753cd1be29a8e3d9986db2
Michael Bestas <mkbestas@lineageos.org>
2025-11-21 07:10:09 +0530

    salami: Depend on android.hardware.graphics.common-V7
    Required for BP3A.
    
    Change-Id: Id071b20aa56d5c2e2717215fa6bb6f16f91198ec
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- extract-files.py
+++ extract-files.py
@@ -48,7 +48,7 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('(NXPLOG_.*_LOGLEVEL)=0x03', '\\1=0x02')
         .regex_replace('NFC_DEBUG_ENABLED=1', 'NFC_DEBUG_ENABLED=0'),
     'odm/lib64/libAlgoProcess.so': blob_fixup()
-        .replace_needed('android.hardware.graphics.common-V3-ndk.so', 'android.hardware.graphics.common-V6-ndk.so'),
+        .replace_needed('android.hardware.graphics.common-V3-ndk.so', 'android.hardware.graphics.common-V7-ndk.so'),
     (
         'odm/lib64/libCOppLceTonemapAPI.so',
         'odm/lib64/libCS.so',
