188991115c6e2f56ddf44a094887d2318c674314
Bruno Martins <bgcngm@gmail.com>
2025-11-29 23:58:11 +0000

    salami: Drop LiveDisplay anti-flicker feature
    It's not supported after all.
    
    Change-Id: Ie76547d231d59ca0f395e8bb892cae5476f13eb7

--- device.mk
+++ device.mk
@@ -24,9 +24,6 @@ PRODUCT_COPY_FILES += \
 # Fingerprint
 $(call soong_config_set,surfaceflinger,udfps_lib,//hardware/oplus:libudfps_extension.oplus)
 
-# LiveDisplay
-$(call soong_config_set,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_AF,true)
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
