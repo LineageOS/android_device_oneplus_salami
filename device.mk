37fff7659faa0b2ad8afcaf6bda5fa605f6295b3
Bruno Martins <bgcngm@gmail.com>
2025-12-27 23:25:43 +0000

    salami: Build Pixelworks interfaces
    Change-Id: Ife6d6ffc2b8e68672e7bdab72d7852dd8d34e73c

--- device.mk
+++ device.mk
@@ -58,7 +58,8 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/pixelworks/interfaces
 
 # Telephony
 PRODUCT_PACKAGES += \
