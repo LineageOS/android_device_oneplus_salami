8a46b0517676631524ae87493bb48f0de74d887b
Bruno Martins <bgcngm@gmail.com>
2026-01-19 00:21:31 +0000

    salami: Create standalone RRO for KeyHandler
    Change-Id: Ia6867ce4443de4edfd54dc61db5550fc3a59ccd8

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2025 The LineageOS Project
+# Copyright (C) 2021-2026 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -31,6 +31,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     FrameworksResEuicc_EU \
     FrameworksResEuicc_NA \
+    KeyHandlerResTarget \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSettingsResTarget \
